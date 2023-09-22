91cf5854dcb73d5c8ad2fbc85e575eea584ea6c5
SeHee JU <kathy1221@naver.com>
2023-09-22 14:38:06 +0900

    FlaskWeb

--- 07.FlaskWeb/04.static.py
+++ 07.FlaskWeb/04.static.py
@@ -1,18 +1,19 @@
-import os 
-from flask import Flask, render_template
+import os
+from flask import Flask, render_template, request
 
-app = Flask(__name__)
-                    # htp://127.0.0.1:5000/ (/ 생략)
-@app.route('/')     # localhost:5000/ 을 서비스하기 위한 코드 
+app = Flask(__name__) 
+
+@app.route('/')
 def index():
     return '<h1>Static Resource</h1>'
 
 @app.route('/static')
-def static_recource():
-    print(app.root_path)            # D:/WorkSpace/02.DataAnalysis/07.FlaskWeb
-    img_file = os.path.join(app.root_path, 'static/img/cat.jpg')  # FlaskWab 에 Static 가 있는 곳이 root 임
-    return render_template('04.static.htma')
+def static_resource():                      # 함수명에는 키워드이름으로  쓰면 에러!
+    print(app.root_path)                    # 해당 파일 위치가 root_path (D:\WorkSpace\02.DataAnalysis\07.FlaskWeb)
+    img_file = os.path.join(app.root_path, 'static/img/cat.jpg')
+    # static resource가 Cache로 인하여 즉시 변경이 안되는 경우도 있음
+    mtime = int(os.stat(img_file).st_mtime)      # 마지막으로 변경된 시간
+    return render_template('04.static.html', mtime=mtime)       # html에서 mtime을 변수로 사용가능
 
-
- if __name__ == '__main__':
-    app.run(debug=True)   
+if __name__ == '__main__':
+    app.run(debug=True)
