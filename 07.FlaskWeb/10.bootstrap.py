851a7aec1813604ed253c99360d7658021276010
SeHee JU <kathy1221@naver.com>
2023-09-26 16:17:16 +0900

    플라스크 부트스트랩

--- 07.FlaskWeb/10.bootstrap.py
+++ 07.FlaskWeb/10.bootstrap.py
@@ -1,21 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template 
 
 app = Flask(__name__)
-                    # htp://127.0.0.1:5000/ (/ 생략)
-@app.route('/')     # localhost:5000/ 을 서비스하기 위한 코드 
+
+@app.route('/')     # localhost:5000/ 을 서비스하기 위한 코드
 def index():
     return '<h1>부트스트랩</h1>'
 
 @app.route('/list')
 def list():
     return render_template('10.list.html')
-    
+
 @app.route('/form')
 def form():
     return render_template('10.form.html')
 
 @app.route('/modal')
-def Modal():
+def modal():
     return render_template('10.modal.html')
 
 if __name__ == '__main__':
